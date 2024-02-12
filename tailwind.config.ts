39ca935c4f38747215659574e6972179d830f3c3
Lucas Mariano dos Santos <lucasmariano2000@hotmail.com>
2024-02-12 14:13:44 -0300

    feat: extended tailwind theme with league spartan font-family

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -3,7 +3,11 @@ import type { Config } from 'tailwindcss'
 export default {
   content: ['./app/**/*.{js,jsx,ts,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        'league-spartan': ['League Spartan', 'sans-serif'],
+      },
+    },
   },
   plugins: [],
 } satisfies Config
